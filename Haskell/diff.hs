bd779bb45a3c1e119634ef8c82cb4c978f9c2d22
Syata Kurasi <soccer_Rs0515@yahoo.co.jp>
2017-10-03 15:35:48 +0900

    created diff.hs

--- Haskell/diff.hs
+++ Haskell/diff.hs
@@ -5,4 +5,15 @@ diff_e f x = (f (x + h) - f (x - h)) / (2 * h)
 diff :: (Double -> Double) -> Double -> Double
 diff f = diff_e f
 
+partial_diff_e :: ([Double] -> Double) -> Int -> [Double] -> Double
+partial_diff_e f i x = (f (ladd x plh) - f (ladd x mlh)) / (2 * h)
+	where h   = 0.00005;
+		  plh = (replicate i 0) ++ [h] ++ (replicate (n-i-1) 0);
+		  mlh = map (\x -> -x) plh;
+		  n   = length x
 
+partial_diff :: ([Double] -> Double) -> Int -> [Double] -> Double
+partial_diff f i = partial_diff_e f i
+
+ladd :: (Num a) => [a] -> [a] -> [a]
+ladd x y = sum $ map (\(l1,l2) -> l1 + l2) (zip x y)
