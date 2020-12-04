5e50a9e5dc585b965a53b25b2665d5c45d81cce6
ShataKurashi <soccer.271828.339@gmail.com>
2020-12-05 00:02:10 +0900

    p.156

---
+++ Rust/samplecli/src/main.rs
@@ -0,0 +1,37 @@
+use clap::Clap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+#[derive(Clap, Debug)]
+#[clap(
+    name = "My RPN program",
+    version = "1.0.0",
+    author = "Your name",
+    about = "Super awesome sample RPN calculator"
+)]
+struct Opts {
+    /// Sets the level of verbosity
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>,
+}
+
+fn main () {
+    let opts = Opts::parse();
+
+    if let Some(path) = opts.formula_file {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+
+        for line in reader.lines() {
+            let line = line.unwrap();
+            println!("{}", line);
+        }
+    } else {
+        // Not specified a file
+        println!("No file is specified.");
+    }
+}
