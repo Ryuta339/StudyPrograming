8ee6ad67bcbb811108f1e78de64c9dc55c7ab9b2
ShataKurashi <soccer.271828.339@gmail.com>
2020-12-24 19:47:59 +0900

    go docker

--- WebApplication/go/server.go
+++ WebApplication/go/server.go
@@ -1,26 +1,101 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"log"
-	"net/http"
-	"net/http/httputil"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	dump, err := httputil.DumpRequest(r, true)
+const (
+	DOCUMENT_ROOT = "template"
+)
+
+func writeLine(w *bufio.Writer, str string) error {
+	_, err := w.WriteString(str + "\n")
 	if err != nil {
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+		return err
+	}
+	err = w.Flush()
+	return err
+}
+
+func run(conn net.Conn) {
+	var path string
+	var ext string
+
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	line, err := reader.ReadString('\n')
+	for err == nil {
+		if line == "" || line == "\r\n" {
+			break
+		}
+		if strings.HasPrefix(line, "GET") {
+			path = strings.Split(line, " ")[1]
+			tmp := strings.Split(path, ".")
+			ext = tmp[len(tmp)-1]
+		}
+		line, err = reader.ReadString('\n')
+	}
+	fmt.Println(path, ext)
+	// Write Header
+	writer := bufio.NewWriter(conn)
+	writeLine(writer, "HTTP/1.1 200 OK")
+	writeLine(writer, "Date: "+getDateStringUtc())
+	writeLine(writer, "Server: Modoki/0.1")
+	writeLine(writer, "Connection: close")
+	writeLine(writer, "Content-Type: "+getContentType(ext))
+	writeLine(writer, "")
+
+	// Write Body
+	fp, err := os.Open(DOCUMENT_ROOT + path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
 		return
 	}
-	fmt.Println(string(dump))
-	fmt.Fprintf(w, "<html><body>hello</body></html>")
+	defer fp.Close()
+	writer.ReadFrom(fp)
+	writer.Flush()
+}
+
+func getDateStringUtc() string {
+	return time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05") + " GMT"
+}
+
+var contentTypeMap map[string]string = map[string]string{
+	"html": "text/html",
+	"htm":  "text/htm",
+	"txt":  "text/plain",
+	"css":  "text/css",
+	"jpeg": "image/jpeg",
+	"jpg":  "image/jpg",
+	"png":  "image/png",
+	"gif":  "image/gif",
+}
+
+func getContentType(ext string) (ret string) {
+	ret = contentTypeMap[ext]
+	if ret == "" {
+		ret = "application/octet-stream"
+	}
+	return
 }
 
 func main() {
-	var httpServer http.Server
-	http.HandleFunc("/", handler)
-	log.Println("start http listening :8001")
-	httpServer.Addr = ":8001"
-	log.Println(httpServer.ListenAndServe())
+	listener, err := net.Listen("tcp", "0.0.0.0:8001")
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		fmt.Println("Server running at 0.0.0.0:8001")
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		run(conn)
+	}
 }
